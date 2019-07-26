58b25ca7825861369d88204608f4377011d64ec7
Rogerio <rgoandrade@gmail.com>
2019-07-25 21:25:08 -0300

    acrescentado mais um append

--- repos/TravelPost/Program.cs
+++ repos/TravelPost/Program.cs
@@ -7,9 +7,9 @@ namespace TravelPost
     {
         static void Main(string[] args)
         {
-
             Comment c1 = new Comment("Have a nice trip !");
-            Comment c2 = new Comment("Wow tha's awesome !");
+            Comment c2 = new Comment("Wow tha's awesomexxx !");
+            Comment c5 = new Comment("Teste hoje !");
 
             Post p1 = new Post(
                 DateTime.Parse("21/06/2018 13:05:44"),
@@ -20,10 +20,12 @@ namespace TravelPost
 
             p1.AddComment(c1);
             p1.AddComment(c2);
+            p1.AddComment(c5);
 
 
             Comment c3 = new Comment("Good Night !");
             Comment c4 = new Comment("My the Force be with you !");
+            Comment c6 = new Comment("teste de hoje");
 
             Post p2 = new Post(
                 DateTime.Parse("28/07/2018 23:14:19"),
@@ -34,9 +36,10 @@ namespace TravelPost
 
             p2.AddComment(c3);
             p2.AddComment(c4);
+            p2.AddComment(c6);
 
             Console.WriteLine(p1);
-            Console.WriteLine(p2);
+            Console.WriteLine(p2);            
         }
     }
 }
