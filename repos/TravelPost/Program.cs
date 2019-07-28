f0b635bfb4bf2f0be10b12a0b8987aa31d89a57c
Rogerio <rgoandrade@gmail.com>
2019-07-27 23:09:56 -0300

    atualização da classe principal

--- repos/TravelPost/Program.cs
+++ repos/TravelPost/Program.cs
@@ -1,4 +1,6 @@
-﻿using TravelPost.Entities;
+﻿
+using System;
+using TravelPost.Entities;
 
 namespace TravelPost
 {
@@ -9,6 +11,7 @@ namespace TravelPost
             Comment c1 = new Comment("Have a nice trip !");
             Comment c2 = new Comment("Wow tha's awesomexxx !");
             Comment c5 = new Comment("Teste hoje !");
+            Comment c7 = new Comment("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
 
             Post p1 = new Post(
                 DateTime.Parse("21/06/2018 13:05:44"),
@@ -20,27 +23,30 @@ namespace TravelPost
             p1.AddComment(c1);
             p1.AddComment(c2);
             p1.AddComment(c5);
-
+            p1.AddComment(c7);
 
 
 
             Comment c3 = new Comment("Good Night !");
             Comment c4 = new Comment("My the Force be with you !");
             Comment c6 = new Comment("teste de hoje");
+            Comment c8 = new Comment("yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy");
 
             Post p2 = new Post(
                 DateTime.Parse("28/07/2018 23:14:19"),
                 "Good night guys",
                 "See you tomorrow !",
-                10
+                5
                 );
 
             p2.AddComment(c3);
             p2.AddComment(c4);
             p2.AddComment(c6);
+            p2.AddComment(c8);
+
 
             Console.WriteLine(p1);
-            //Console.WriteLine(p2);            
+            Console.WriteLine(p2);            
         }
     }
 }
